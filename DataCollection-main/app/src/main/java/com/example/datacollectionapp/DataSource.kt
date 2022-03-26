9d11efb23a2b458ead75b63b37e719fee0cf9eab
Duke <dukemwanzia@gmail.com>
2022-03-26 13:58:12 +0300

    did some ui updates

---
+++ DataCollection-main/app/src/main/java/com/example/datacollectionapp/DataSource.kt
@@ -0,0 +1,23 @@
+package com.example.datacollectionapp
+
+data class County(
+    val countyNumber: Int,
+    val countyName: String
+)
+class DataSource {
+    fun loadCounties(): List<County>{
+        return listOf<County>(
+            County(1, "Mombasa"),
+            County(2, "Kwale"),
+            County(3, "Kilifi"),
+            County(4, "Tana River"),
+            County(5,"Lamu"),
+            County(6, "Taita Taveta"),
+            County(7, "Garrissa"),
+            County(8, "Wajir"),
+            County(9, "Mandera"),
+            County(10, "Marsabit"),
+            County(11, "Isiolo"),
+        )
+    }
+}
